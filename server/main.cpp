c099d2807e6d310ac43e7592fa9eeda3b91160eb
microcai <microcaicai@gmail.com>
2013-04-04 10:32:40 +0800

    add signal process

--- server/main.cpp
+++ server/main.cpp
@@ -5,6 +5,9 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 
+#include <protocol/avim-base.pb.h>
+#include <protocol/avim-im.pb.h>
+
 // 创建 avim::im::server 对象咯.
 static void avim_server_accept_new_client(boost::shared_ptr<boost::asio::ip::tcp::socket> socket)
 {
@@ -14,9 +17,17 @@ static void avim_server_accept_new_client(boost::shared_ptr<boost::asio::ip::tcp
 int main(int argc, char **argv)
 {
 	boost::asio::io_service io_service;
-	boost::asio::io_service::work work(io_service);
+
+	boost::asio::signal_set signal_set(io_service);
+	signal_set.add(SIGINT);
+	signal_set.add(SIGTERM);
+#ifdef SIGHUP
+	signal_set.add(SIGHUP);
+#endif
+	signal_set.async_wait(boost::bind(&boost::asio::io_service::stop, boost::ref(io_service)));
+
     unsigned int num_of_cpus =  boost::thread::hardware_concurrency();
-	for (int i=0 ; i <num_of_cpus ; i++)
+    for (int i=0 ; i <num_of_cpus ; i++)
 		boost::thread(boost::bind(&boost::asio::io_service::run, boost::ref(io_service)));
 
 	std::cout << "Hello, world!" << std::endl;
