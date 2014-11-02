da6c2ac80da73f95a3f3bcccc3fca2f5b2cf6e5a
microcai <microcaicai@gmail.com>
2014-11-02 23:27:17 +0800

    stub for jobs

--- lib/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
-
-typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socketptr;
---
+++ libavproto/src/avcore.cpp
@@ -0,0 +1,12 @@
+
+#include <avproto.hpp>
+
+void av_start()
+{
+
+}
+
+bool av_config(std::string pkey, std::string  cert)
+{
+	return false;
+}
