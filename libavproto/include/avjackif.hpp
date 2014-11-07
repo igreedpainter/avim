084b6bebaa6190cab93d8ccaa5a63ca5dac58970
microcai <microcaicai@gmail.com>
2014-11-08 00:34:04 +0800

    dh 交换

--- libavproto/include/avjackif.hpp
+++ libavproto/include/avjackif.hpp
@@ -9,11 +9,12 @@
 // 这个是 和 JACK 写的 router 对接的接口
 struct avjackif : boost::noncopyable
 {
+	std::vector<unsigned char> m_shared_key;
 public:
 	avjackif(boost::asio::io_service & _io_service);
 	~avjackif();
 
-	viud set_pki(RSA * _key, X509 *);
+	void set_pki(RSA * _key, X509 *);
 
 	// TCP接口，有 master/slave 模式之分 服务器使用 master 模式，客户端则是 slave 模式
 	// 区别只是登录时候的握手不一样，所以就在 handshake 这里直接设定就可以了
@@ -27,14 +28,14 @@ public:
 public: // 下面是实现 avif 接口
 	boost::asio::io_service & get_io_service() const;
 	std::string get_ifname() const;
-	const proto::base::avAddress * if_address() const;
-	const proto::base::avAddress * remote_address() const;
+	const proto::avAddress * if_address() const;
+	const proto::avAddress * remote_address() const;
 	RSA * get_rsa_key();
 	X509 * get_cert();
 
 	void set_root_ca(X509 * ca);
-	boost::shared_ptr<proto::base::avPacket> async_read_packet(boost::asio::yield_context yield_context);
-    bool async_write_packet(proto::base::avPacket*, boost::asio::yield_context yield_context);
+	boost::shared_ptr<proto::avPacket> async_read_packet(boost::asio::yield_context yield_context);
+    bool async_write_packet(proto::avPacket*, boost::asio::yield_context yield_context);
 
 protected:
 	bool check_cert(const std::string & cert);
@@ -46,10 +47,10 @@ private:
 	std::string ifname;
 	RSA * _rsa;
 	X509 * _x509, *m_root_ca;
-	boost::scoped_ptr<proto::base::avAddress> m_local_addr;
-	boost::scoped_ptr<proto::base::avAddress> m_remote_addr;
-	boost::shared_ptr<boost::asio::ip::tcp::socket> m_sock;
+	boost::scoped_ptr<proto::avAddress> m_local_addr;
+	boost::scoped_ptr<proto::avAddress> m_remote_addr;
 
+	boost::asio::ip::tcp::socket m_sock;
 	boost::asio::streambuf m_recv_buf, m_send_buf;
 };
 
--- libavproto/src/avjackif.cpp
+++ libavproto/src/avjackif.cpp
@@ -5,22 +5,53 @@
 
 #include "avjackif.hpp"
 
+#include "avim_proto/message.pb.h"
+#include "avim_proto/user.pb.h"
 
 /*
  * 这个类呢，是用来实现和 JACK 实现的那个 AVROUTER 对接的。也就是 JACK 版本的 AV NETWORK SERVICE PROVIDER
  */
 
 // av地址可以从证书里获取，所以外面无需传递进来
-avjackif::avjackif(boost::asio::io_service & _io_service,
-	std::string login_username, std::string login_password, RSA * _key, X509 *)
+avjackif::avjackif(boost::asio::io_service & _io_service)
+	: m_sock(_io_service)
 {
 
 }
 
 void avjackif::async_handshake(std::string login_username, std::string login_password, boost::asio::yield_context yield_context)
 {
+	proto::client_hello client_hello;
+	client_hello.set_client("avim");
+	client_hello.set_version(0001);
+
+	unsigned char to[100];
+
 	auto dh = DH_new();
-	DH_generate_parameters();
+	DH_generate_parameters_ex(dh,64,DH_GENERATOR_5,NULL);
+
+	// 把 5 传过去
+	client_hello.set_random_key((const void*)to, BN_bn2bin(dh->g, to));
+
+	auto tobesend = av_router::encode(client_hello);
+
+	boost::asio::async_write(m_sock, boost::asio::buffer(tobesend), yield_context);
+
+	std::uint32_t l;
+	boost::asio::async_read(m_sock, boost::asio::buffer(&l, sizeof(l)), yield_context);
+
+	std::string  buf;
+	buf.resize(htonl(l));
+	boost::asio::async_read(m_sock, boost::asio::buffer(&buf[0], htonl(l)), yield_context);
+
+	// 解码
+	proto::server_hello * server_hello = (proto::server_hello *) av_router::decode(buf);
+
+	dh->p =BN_bin2bn((const unsigned char *) server_hello->random_key().data(), server_hello->random_key().length(), dh->p);
 	DH_generate_key(dh);
 
+	m_shared_key.resize(DH_size(dh));
+	// 密钥就算出来啦！
+	DH_compute_key(&m_shared_key[0], dh->pub_key, dh);
+	DH_free(dh);
 }
