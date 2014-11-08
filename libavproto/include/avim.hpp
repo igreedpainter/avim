f141f2fa515546d03bdbbba0c2146db04911f0ba
microcai <microcaicai@gmail.com>
2014-11-09 01:37:37 +0800

    pull in avim protocol change

--- libavproto/include/avim.hpp
+++ libavproto/include/avim.hpp
@@ -1,13 +1,27 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <memory>
+#include "avim_proto/address.pb.h"
+#include "avim_proto/im.pb.h"
 
 // 呐, 这个才是真正真正的 avim 协议实现. 这类主要处理 IM 层协议
 class avim
 {
-	boost::asio::io_service & io_service;
-
 public:
-	avim(boost::asio::io_service &);
+	boost::function<void(boost::system::error_code, proto::avAddress, proto::avim_packet)> RecvHandlerType;
+	boost::function<void(boost::system::error_code)> SendHandlerType;
+public:
+	// 使用密钥和证书构建 avim 对象
+	avim(boost::asio::io_service &, std::string privatekey, std::string public_cert);
+
+
+	// 异步消息接收
+	void async_recv_im(RecvHandlerType);
+	void async_send_im(proto::avAddress, proto::avim_packet, SendHandlerType);
+	void async_send_im(proto::avAddress, proto::avim_packet, int flag, SendHandlerType);
+private:
+	boost::asio::io_service & io_service;
 };
 
