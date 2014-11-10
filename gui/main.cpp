9939a89fe5f60d3f636080af6e728edf6c832a85
microcai <microcaicai@gmail.com>
2014-11-10 22:01:03 +0800

    自动创建软件目录

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,5 +1,8 @@
 #include <openssl/evp.h>
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 
+#include <QtCore/QStandardPaths>
 #include <QtWidgets/QApplication>
 #include "gavim.h"
 
@@ -7,7 +10,14 @@ int main(int argc, char *argv[])
 {
 	OpenSSL_add_all_algorithms();
 	QApplication a(argc, argv);
+
+	fs::path appdatadir = QStandardPaths::standardLocations(QStandardPaths::DataLocation).first().toStdString();
+
+	if(!fs::exists(appdatadir))
+		fs::create_directories(appdatadir);
+
 	gavim w;
+
 	w.show();
 	return a.exec();
 }
